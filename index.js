1d888de26e766a86c2a0ccba15be8a2062883c9b
Maksym04u <101353357+Maksym04u@users.noreply.github.com>
2024-11-21 01:04:53 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,2 +1,7 @@
+const app = require('./server'); // Import the app from server.js
+
+// If you need to run the server here (in index.js)
 const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
+app.listen(PORT, () => {
+    console.log(`Server running at http://localhost:${PORT}`);
+});
